07cf34ecb92ab4a3a0bf67af76d02736f9784c9b
Ykro314 <lolkokpol@gmal.com>
2015-09-25 22:57:59 +0300

    готова разметка, сролл-плагин на jquery

---
+++ js/function.js
@@ -0,0 +1,9 @@
+$("a.toplink").click(function() {
+    $("html, body").animate({
+       scrollTop: $($(this).attr("href")).offset().top + "px"
+    }, {
+       duration: 500,
+       easing: "swing"
+    });
+    return false;
+ });
